c9845ff555629f91e56eb9e46b80fa15addebbcd
Mounika <bojjamounika8@gmail.com>
2021-12-28 18:41:32 +0530

    functional

---
+++ src/App.js
@@ -0,0 +1,45 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+//function App() {
+  //return (
+    //<div className="App">
+      //<header className="App-header">
+        //<img src={logo} className="App-logo" alt="logo" />
+        //<p>
+          //Edit <code>src/App.js</code> and save to reload.
+        //</p>
+        //<a
+          //className="App-link"
+          //href="https://reactjs.org"
+          //target="_blank"
+          //rel="noopener noreferrer"
+        //>
+          //Learn React
+        //</a>
+      //</header>
+    //</div>
+  //);
+//}
+
+//export default App;
+
+import React, { useState, useEffect } from 'react';
+
+function Example() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Like
+      </button>
+    </div>
+  );
+}
+
+export default Example;
